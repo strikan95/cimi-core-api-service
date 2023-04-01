3745774b93f58cd0938e3b6000c5bff3f4e2e85f
strikan <buljevic.juraj@gmail.com>
2023-04-01 15:12:23 +0200

    initial auth0 setup

--- config/bundles.php
+++ config/bundles.php
@@ -4,4 +4,5 @@ return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
+    Auth0\Symfony\Auth0Bundle::class => ['all' => true],
 ];
