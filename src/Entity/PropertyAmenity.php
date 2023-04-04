18ae1a70537929754f5791c166b9bb8d9a4f7aa7
strikan <buljevic.juraj@gmail.com>
2023-04-04 17:15:29 +0200

    added entities for amenity and property listing

---
+++ src/Entity/PropertyAmenity.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\PropertyAmenityRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: PropertyAmenityRepository::class)]
+class PropertyAmenity
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 32)]
+    private ?string $name = null;
+
+    #[ORM\ManyToMany(targetEntity: PropertyListing::class, mappedBy: 'amenities')]
+    private Collection $listings;
+
+    public function __construct()
+    {
+        $this->listings = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getListings(): Collection
+    {
+        return $this->listings;
+    }
+
+    public function setListings(Collection $listings): void
+    {
+        $this->listings = $listings;
+    }
+}
---
+++ src/Entity/PropertyListing.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\PropertyListingRepository;
+use DateTime;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: PropertyListingRepository::class)]
+class PropertyListing
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $title = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $description = null;
+
+    #[ORM\Column(name: "createdAt", type: "datetime")]
+    private ?DateTime $createdAt = null;
+
+    #[ORM\ManyToMany(targetEntity: PropertyAmenity::class, inversedBy: 'listings')]
+    #[ORM\JoinTable(name: 'listings_amenities')]
+    private Collection $amenities;
+
+    public function __construct()
+    {
+        $this->amenities = new ArrayCollection();
+        $this->createdAt = new DateTime();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): self
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?DateTime
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(?DateTime $createdAt): void
+    {
+        $this->createdAt = $createdAt;
+    }
+
+    /**
+     * @return Collection
+     */
+    public function getAmenities(): Collection
+    {
+        return $this->amenities;
+    }
+
+    /**
+     * @param Collection $amenities
+     */
+    public function setAmenities(Collection $amenities): void
+    {
+        $this->amenities = $amenities;
+    }
+}
