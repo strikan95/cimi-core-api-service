4cb9adf98c8ef53cf8013cf79c7de7401cb3898f
strikan <buljevic.juraj@gmail.com>
2023-06-07 15:35:30 +0200

    fixtures za testne podatke

---
+++ src/DataFixtures/AmenityFixtures.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Amenity\Entity\Amenity as AmenityEntity;
+use Doctrine\Persistence\ObjectManager;
+
+class AmenityFixtures extends BaseFixture
+{
+    private static array $amenityNames = [
+        'Wifi',
+        'Washing machine',
+        'Dishwasher',
+        'Smart TV',
+        'Kitchen',
+        'Dryer',
+        'Air Conditioning',
+        'Heating'
+    ];
+
+    protected function loadData(ObjectManager $manager): void
+    {
+        $this->createMany(AmenityEntity::class, count(self::$amenityNames), function (AmenityEntity $amenity, $count) {
+            $amenity->setName(self::$amenityNames[$count]);
+        });
+
+        $manager->flush();
+    }
+}
---
+++ src/DataFixtures/BaseFixture.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\DataFixtures;
+
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Exception;
+use Faker\Factory;
+use Faker\Generator;
+
+abstract class BaseFixture extends Fixture
+{
+    /** @var ObjectManager */
+    private ObjectManager $manager;
+    protected Generator $faker;
+
+    private array $referencesIndex = [];
+
+    abstract protected function loadData(ObjectManager $manager);
+    public function load(ObjectManager $manager): void
+    {
+        $this->manager = $manager;
+        $this->faker = Factory::create();
+        $this->loadData($manager);
+    }
+
+    protected function createMany(string $className, int $count, callable $factory): void
+    {
+        for ($i = 0; $i < $count; $i++) {
+            $entity = new $className();
+            $factory($entity, $i);
+            $this->manager->persist($entity);
+            // store for usage later as App\Entity\ClassName_#COUNT#
+            $this->addReference($className . '_' . $i, $entity);
+        }
+    }
+
+    /**
+     * @throws Exception
+     */
+    protected function getRandomReference(string $className)
+    {
+        if (!isset($this->referencesIndex[$className])) {
+            $this->referencesIndex[$className] = [];
+            foreach ($this->referenceRepository->getReferencesByClass() as $key => $ref) {
+                $this->referencesIndex[$key] = array_keys($ref);
+            }
+        }
+
+        if (empty($this->referencesIndex[$className])) {
+            throw new Exception(sprintf('Cannot find any references for class "%s"', $className));
+        }
+
+        $randomReferenceKey = $this->faker->randomElement($this->referencesIndex[$className]);
+        return $this->getReference($randomReferenceKey);
+    }
+}
